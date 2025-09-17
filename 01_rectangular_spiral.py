a775577b02eb51511221293770856f39dec59bd0
Hummingliberty616 <wlin2028@kehillahstudent.org>
2025-09-17 11:53:24 -0700

    -

--- 01_rectangular_spiral.py
+++ 01_rectangular_spiral.py
@@ -6,6 +6,9 @@ Make a rectangular spiral (see the README.md for an example)
 
 ### YOUR CODE STARTS HERE
 
+for i in range(1,100):
+    turtle.forward(50*i)
+    turtle.right(90)
 
 ### YOUR CODE ENDS HERE
 
